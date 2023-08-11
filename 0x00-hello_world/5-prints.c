0a57a3daa1bf62a194400b432a0a0f3ec33f588d
Osinaike1 <osianikeoluwafunmilayo@gmail.com>
2023-08-11 15:44:06 -0700

    cis

--- 0x00-hello_world/5-prints.c
+++ 0x00-hello_world/5-prints.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - ENTRY POINT
+ * main - this is the main function
  * Return: always 0 (success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
-
 
